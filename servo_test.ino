8596d6c4b0a4cf40fa2e5a94d0daaa8e520f1ec9
alfa934 <alfarras934@gmail.com>
2023-05-28 08:37:46 +0700

    Update servo_test.ino

--- servo_test.ino
+++ servo_test.ino
@@ -2,33 +2,57 @@
 #include "ros.h"
 #include "std_msgs/Int32MultiArray.h"
 
-Servo myServo;
+Servo servoX, servoY;
 
 // SETUP ROS
 ros::NodeHandle nh;
 
+// SERVO VARIABLES
 int middle = 90;
 int rightMax = 180;
 int leftMax = 0;
-
+int upMax = 45;
+int downMax = 135;
 int posX = middle;
+int posY = middle;
 int dx = 4;
+int dy = 4;
+
 void messageCb(const std_msgs::Int32MultiArray& pos_msg)
 {
+  // elements of the arrays (only has 2)
   // X = pos_msg.data[0]
   // Y = pos_msg.data[1]
   
-  if(pos_msg.data[0] > 540)
+  int X = pos_msg.data[0];
+  int Y = pos_msg.data[1];
+  
+  /* This is for the "X" axis servo */
+  if(X < 100)
   {
     if(posX <= rightMax)
       posX += dx;
   }
-  else if(pos_msg.data[0] < 100)
+  else if(X > 540)
   {
     if(posX >= leftMax)
       posX -= dx;
   }
-  myServo.write(posX);
+  // This is for the Y axis servo
+  if(Y < 100) 
+  {
+    if(posY <= downMax)
+      posY += dy;
+  }
+  else if(Y > 380)
+  {
+    if(posY >= upMax)
+      posY -= dy;
+  }
+    
+  servoX.write(posX);
+  servoY.write(posY);
+
   delay(10);
 }
 
@@ -36,10 +60,12 @@ ros::Subscriber<std_msgs::Int32MultiArray> sub( "servo_pos", &messageCb );
 
 void setup() {
   // put your setup code here, to run once:
-  myServo.attach(9);
+  servoX.attach(9);
+  servoY.attach(10);
   nh.initNode();
   nh.subscribe(sub);
-  myServo.write(middle);
+  servoX.write(middle);
+  servoY.write(middle);
 }
 
 void loop()  {
