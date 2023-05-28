afaf409c9a3797b643f622d66bffd4304a6ebe64
alfa934 <alfarras934@gmail.com>
2023-05-28 08:18:26 +0700

    Update blobCamera.cpp

--- blobCamera.cpp
+++ blobCamera.cpp
@@ -1,44 +1,15 @@
-// INCLUDE THIS IN YOUR OWN ROS PACKAGE..
-// OTHERWISE DELETE ROS HEADERS AND SYNTAX
 #include <opencv2/opencv.hpp>
-// #include "ros/ros.h"
-// #include "std_msgs/Int32MultiArray.h"
+#include "ros/ros.h"
+#include "std_msgs/Int32MultiArray.h"
 #include <iostream>
 
 // RESOLUTION WINDOW 640 x 480
 
 using namespace cv;
 
-int main(int argc, char **argv)
+SimpleBlobDetector::Params params;
+void SetupBlobDetector()
 {
-    /********************************** Ros Initialisation **********************************/
-    
-    // ros::init(argc, argv, "blobCamera");
-
-    // ros::NodeHandle n;
-
-    // ros::Publisher pos_pub = n.advertise<std_msgs::Int32MultiArray>("servo_pos", 1000);
-    
-    // std_msgs::Int32MultiArray pos_msg;
-
-    // ros::Rate loop_rate(10);
-
-
-    /************************************** OpenCV stuff **************************************/
-    
-    // Open camera and check if it is available
-    VideoCapture cap(0);
-
-    if(!cap.isOpened())
-    {
-        std::cout << "Error opening video stream" << std::endl;
-        return -1;
-    }
-    
-
-    /********************* SimpleBlobDetector Setup (parameters, etc...) *********************/
-
-    SimpleBlobDetector::Params params;
     // Filter by color
     params.filterByColor = true;
     params.blobColor = 255; // White blobs
@@ -55,6 +26,38 @@ int main(int argc, char **argv)
     // Filter by inertia
     params.filterByInertia = false;
     params.minInertiaRatio = 0.01;
+}
+
+int main(int argc, char **argv)
+{
+    /********************************** Ros Initialisation **********************************/
+    
+    ros::init(argc, argv, "blobCamera");
+
+    ros::NodeHandle n;
+
+    ros::Publisher pos_pub = n.advertise<std_msgs::Int32MultiArray>("servo_pos", 1000);
+    
+    std_msgs::Int32MultiArray pos_msg;
+
+    ros::Rate loop_rate(10);
+
+
+    /************************************** OpenCV stuff **************************************/
+    
+    // Opens the camera and check if it is available
+    VideoCapture cap(0);
+
+    if(!cap.isOpened())
+    {
+        std::cout << "Error opening video stream" << std::endl;
+        return -1;
+    }
+    
+
+    /********************* SimpleBlobDetector Setup (parameters, etc...) *********************/
+    
+    SetupBlobDetector();
     // initialise the blob detector
     Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
 
@@ -72,7 +75,7 @@ int main(int argc, char **argv)
 
         /******************************** Frame Manipulation ********************************/
         
-        // draws rectangle in output frames
+        // show rectangle/square thing
         Rect rect(100, 100, frame.cols-200, frame.rows-200);
         rectangle(flippedFrame, rect, Scalar(0, 255 ,0), 2);
 
@@ -80,10 +83,16 @@ int main(int argc, char **argv)
         Mat hsv;
         cvtColor(flippedFrame, hsv, COLOR_BGR2HSV);
 
-        // Threshold the image based on color
+        // Threshold the image based on color, try to calibrate for better results
+        int low_H  =   0, 
+            low_S  =  80, 
+            low_V  = 140;
+        int high_H = 180, 
+            high_S = 255, 
+            high_V = 255;
         Mat mask;     // might need to calibrate Scalar() values
-        inRange(hsv, Scalar(0, 80, 140), Scalar(180, 255, 255), mask);
-        
+        inRange(hsv, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), mask);
+
         // Apply morphological operations to remove noise and fill small gaps in the blobs
         Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 9));
         morphologyEx(mask, mask, MORPH_OPEN, kernel);
@@ -110,10 +119,10 @@ int main(int argc, char **argv)
                     int x = (int)maxPt.x;
                     int y = (int)maxPt.y;
 
-                    // pos_msg.data.clear();
-                    // pos_msg.data.push_back(x);
-                    // pos_msg.data.push_back(y);
-                    // pos_pub.publish(pos_msg);
+                    pos_msg.data.clear();
+                    pos_msg.data.push_back(x);
+                    pos_msg.data.push_back(y);
+                    pos_pub.publish(pos_msg);
 
                     printf("X,Y : [ %d, %d ] \n", x, y);
                 }   
@@ -121,10 +130,10 @@ int main(int argc, char **argv)
         }
         
         // Draw detected blobs as circles
-        Mat im_with_keypoints;
+        Mat img_keypoints;
         drawKeypoints(  flippedFrame, 
                         keypoints, 
-                        im_with_keypoints, 
+                        img_keypoints, 
                         Scalar(0, 0, 255), 
                         DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
         
@@ -132,8 +141,8 @@ int main(int argc, char **argv)
         /******************************** SHOW FRAMES ********************************/
         
         // Show the image with keypoints
-        imshow("Blob detection", im_with_keypoints);
-        // Exit on ESC key
+        imshow("Blob detection", img_keypoints);
+        
         char c = (char)waitKey(25);
         if(c == 27 || c == 'q')
             break;
@@ -145,3 +154,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
